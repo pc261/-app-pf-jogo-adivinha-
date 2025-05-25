b7997ec409f442911f9e03cf0edf88cb3bf88d39
unknown <santosfelipeliipe@gmail.com>
2025-05-25 12:06:06 -0300

    Finalização da estrutura de sorteio e correção de respostas do jogo

--- Js/app-pf-jogo-adivinha .js
+++ Js/app-pf-jogo-adivinha .js
@@ -1,30 +1,61 @@
-console.log("Hello World");
+console.log("Jogo de Adivinhação iniciado");
 
-
-const guessIput = document.getElementById("guessInput");
+const guessInput = document.getElementById("guessInput");
 const submit = document.getElementById("submit");
-const attempts = document.getElementById("attempts"); 
+const reset = document.getElementById("reset");
+const attempts = document.getElementById("attempts");
 
-guessIput.onclick = () => {
-    if (localStorage.getItem("randomNumber") !== null) {
-        localStorage.getItem("randomNumber");
-        //console.log("O número já foi gerado: " + localStorage.getItem("randomNumber"));
-    } else {
-        const randomNumber = Math.floor(Math.random() * 5) + 1;
-        localStorage.setItem("randomNumber", randomNumber);
-        //console.log(randomNumber);
-    }
+function startGame() {
+    const randomNumber = Math.floor(Math.random() * 100) + 1; // 1 a 100
+    localStorage.setItem("randomNumber", btoa(randomNumber.toString()));
+    localStorage.setItem("attempts", btoa("10"));
+    attempts.innerText = 10;
+}
+
+if (localStorage.getItem("randomNumber") === null) {
+    startGame();
+} else {
+    const storedAttempts = localStorage.getItem("attempts");
+    attempts.innerText = storedAttempts ? atob(storedAttempts) : 10;
 }
 
 submit.onclick = () => {
-    const awnser = Number(localStorage.getItem("randomNumber"));
+    const randomNumberEncoded = localStorage.getItem("randomNumber");
+    const answer = Number(atob(randomNumberEncoded));
+    const guess = Number(guessInput.value);
 
-	if (guessIput.value == awnser) {
-        alert("Parabéns! Você acertou o número!");
-    }else if (guessIput.value < awnser) {
+    if (!guess || guess < 1 || guess > 100) {
+        alert("Por favor, digite um número de 1 a 100.");
+        return;
+    }
+
+    if (guess === answer) {
+        alert("🎉 Parabéns! Você acertou o número!");
+        startGame();
+    } else if (guess < answer) {
         alert("O número é maior!");
     } else {
         alert("O número é menor!");
     }
-    
+
+    let attemptsLeft = Number(atob(localStorage.getItem("attempts")));
+    attemptsLeft--;
+
+    if (attemptsLeft <= 0) {
+        alert("☠️ Suas tentativas acabaram! O número era: " + answer);
+        startGame();
+    } else {
+        localStorage.setItem("attempts", btoa(attemptsLeft.toString()));
+        attempts.innerText = attemptsLeft;
+    }
+
+    guessInput.value = "";
+    guessInput.focus();
+}
+
+reset.onclick = () => {
+    startGame();
+    alert("Jogo reiniciado!");
+    guessInput.value = "";
+    guessInput.focus();
 }
